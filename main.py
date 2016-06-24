96b99e6e73893f608bb693dbf4bbfaa3a14c87b9
tgourdel <thibaut.gourdel@gmail.com>
2016-06-24 11:49:10 +0200

    Automatic downloading of latest video associated to a youtube account

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import subprocess
 import pafy
 
-v = pafy.new("R76jxg-bC18")
+proc = subprocess.Popen(["python", "-c", "import ytsubs; ytsubs.do_it()"], stdout=subprocess.PIPE)
+out = proc.communicate()[0]
+print out
+v = pafy.new(out)
 s = v.getbest()
 print("Size is %s" % s.get_filesize())
 filename = s.download()  # starts download
